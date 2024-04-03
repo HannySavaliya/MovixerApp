8994fa0e8ae4b7e37e793d5b7d65c1f142c63690
SnehaKhunt <snehakhunt4564@gmail.com>
2024-04-03 17:33:22 +0530

    SK | Removed unwanted code

--- API/API.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export function getMoviesAPI() {
-    return axios.get('https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies');
-}
-
--- utils/formikValidation.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup'
-
-export const REGISTRATION_SCHEMA = yup.object({
-    fullname: yup.string().required("Please enter first name"),
-    email: yup.string().email('Invalid email address').required('Please enter email'),
-    password: yup.string().min(6, 'Password must be at least 6 characters').max(8, 'Password must be 8 characters').required('Please enter password'),
-    confirmPassword: yup.string().required("Please enter confirmPassword").oneOf([yup.ref('password'), null], 'Passwords must match'),
-})
